81cebeebc998ece1485ded0ca2ed678c5ea1400e
Alan Jones <dev.alanjones@gmail.coom>
2026-07-20 13:20:12 +0530

    preview

--- src/pages/EditRecipe.jsx
+++ src/pages/EditRecipe.jsx
@@ -40,7 +40,7 @@ const EditRecipe = () => {
         ingredients: data.data.ingredients.join(", "),
         instructions: data.data.instructions.join(", "),
       });
-      setPreview(res.data.data.imgUrl);
+      setPreview(data.data.imgUrl);
     } catch (error) {
       console.log(error);
       alert("Unable to load recipe");
